429e9f54af6bd159bba36c61ffc2544a198aa326
xavierrocks <xruizify@gmail.com>
2016-04-12 22:27:49 -0700

    First website upload

---
+++ js/index.js
@@ -0,0 +1,23 @@
+$( ".navbar-toggle" ).click(function() {
+  $('.topbar').toggleClass( "fadeaway" );
+  $('.topbar').toggleClass( "fadein" );
+  $(".middlebar").toggleClass( "tiltup" );
+  $(".middlebar").toggleClass( "tiltdown" );
+  $(".bottombar").toggleClass( "twistleft" );
+  $(".bottombar").toggleClass( "twistright" );
+});
+
+$(document).ready(function(){
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+});
